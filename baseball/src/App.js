221811d4328d248314c9b5bb463c8ebc4de09978
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-04-09 13:09:40 -0700

    Added basic baseball program

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Display from './Display/Display.js';
+import Dashboard from './Dashboard/Dashboard.js';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    balls: 0,
+    strikes: 0,
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <Display balls={this.state.balls} strikes={this.state.strikes}/>
+        <Dashboard strike={this.strike} ball={this.ball} foul={this.foul} hit={this.hit}/>
+      </>
     );
   }
+
+  strike = () => {
+    if(this.state.strikes === 2 ){
+      this.setState({balls: 0})
+      this.setState({strikes: 0})
+    } else if (this.state.strikes < 2 ){
+      const strike = this.state.strikes + 1; 
+      this.setState({strikes: strike})
+    } 
+
+  };
+
+  ball = () => {
+    if(this.state.balls === 3 ){
+      this.setState({balls: 0})
+      this.setState({strikes: 0})
+    } else if (this.state.balls < 3 ){
+      const ball = this.state.balls + 1; 
+      this.setState({balls: ball})
+    } 
+  };
+
+  foul = () => {
+    if(this.state.strikes === 2 ){
+      return
+    } else if (this.state.strikes < 2 ){
+      const strike = this.state.strikes + 1; 
+      this.setState({strikes: strike})
+    } 
+  };
+
+  hit = () => {
+    this.setState({balls: 0})
+    this.setState({strikes: 0})
+  };
 }
 
 export default App;
---
+++ baseball/src/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export default function Dashboard(props) {
+    return (
+        <>
+        <button onClick={props.strike}>Strike</button>
+        <button onClick={props.ball}>Ball</button>
+        <button onClick={props.foul}>Foul</button>
+        <button onClick={props.hit}>Hit</button>
+        </>
+    )
+  }
