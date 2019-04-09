359a33e6058a2c0b514c76144adcaab6e79f80ce
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-04-09 14:04:18 -0700

    Added more tests for button presses

---
+++ baseball/src/Dashboard/Dashboard.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render, fireEvent } from 'react-testing-library';
+import Dashboard from './Dashboard';
+
+
+describe('<Dashboard />', () => {
+
+  it('renders successfully', () => {
+    render(<Dashboard />);
+  });
+
+});
